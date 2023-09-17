372fbb380a7479f160564c1da49c3a3b527dddbd
Knasten <hampusjojo@gmail.com>
2023-09-17 13:57:43 +0200

    setup layout

--- src/Pages/LayoutPage.jsx
+++ src/Pages/LayoutPage.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
 const LayoutPage = () => {
   return (
     <>
-      <div id="layout">
-        <Outlet />
-      </div>
+      <Navbar />
+      <Outlet />
+      <Footer/>
     </>
   )
 };
